b22194ba162cb046a3c0d55e1dfc8563e984cd70
Oscar Sánchez de Zulueta <osadezu@gmail.com>
2017-02-25 21:07:05 -0500

    Reformat

--- test_shared.py
+++ test_shared.py
@@ -4,56 +4,58 @@ import sys
 from lib.sim900.gsm import SimGsm, SimGsmPinRequestState
 from lib.sim900.imei import SimImeiRetriever
 
+
 def initializeUartPort(
         portName,
-        baudrate = 57600,
-        bytesize = serial.EIGHTBITS,
-        parity   = serial.PARITY_NONE,
-        stopbits = serial.STOPBITS_ONE,
-        timeout  = 0
-    ):
-
+        baudrate=57600,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        timeout=0
+):
     port = serial.Serial()
 
-    #tuning port object
-    port.port         = portName
-    port.baudrate     = baudrate
-    port.bytesize     = bytesize
-    port.parity       = parity
-    port.stopbits     = stopbits
-    port.timeout      = timeout
+    # tuning port object
+    port.port = portName
+    port.baudrate = baudrate
+    port.bytesize = bytesize
+    port.parity = parity
+    port.stopbits = stopbits
+    port.timeout = timeout
 
     return port
 
+
 def initializeLogs(loggerLevel, consoleLoggerLevel):
-    #initializing logging formatter
+    # initializing logging formatter
     formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
 
-    #initializing logger
+    # initializing logger
     logger = logging.getLogger(__name__)
     logger.setLevel(loggerLevel)
 
-    #initializing console handler for logging
+    # initializing console handler for logging
     consoleLogger = logging.StreamHandler(sys.stdout)
     consoleLogger.setLevel(consoleLoggerLevel)
     consoleLogger.setFormatter(formatter)
 
-    #adding console appender
+    # adding console appender
     logger.addHandler(consoleLogger)
 
     return (formatter, logger, consoleLogger,)
 
+
 def baseOperations(port, logger):
-    #class for general functions
+    # class for general functions
     gsm = SimGsm(port, logger)
 
-    #opening COM port
+    # opening COM port
     logger.info("opening port")
     if not gsm.openPort():
         logger.error("error opening port: {0}".format(gsm.errorText))
         return None
 
-    #initializing session with SIM900
+    # initializing session with SIM900
     logger.info("initializing SIM900 session")
     if not gsm.begin(5):
         logger.error("error initializing session: {0}".format(gsm.errorText))
@@ -68,7 +70,7 @@ def baseOperations(port, logger):
     else:
         logger.debug("PIN OK")
 
-    #retrieving IMEI
+    # retrieving IMEI
     sim = SimImeiRetriever(port, logger)
     logger.info("retrieving IMEI")
     imei = sim.getIMEI()
